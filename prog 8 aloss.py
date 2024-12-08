60d88e01d605c40983db5f3cc8b07111f763efe5
aloss07 <ganbarre07@gmail.com>
2024-12-08 11:54:15 +0800

    Add files via upload
    tugas aritmatika pertemuan ke 8

---
+++ prog 8 aloss.py
@@ -0,0 +1,21 @@
+# ARITMATIKA (X : + - ** % //)
+
+a = 24
+b = 23
+
+# penjumlahan
+hasil = a + b
+print ("nilai dari a + b = ", hasil)
+
+# perpangkatan
+hasil = a ** b
+print ("nilai dari a ** b = ", hasil)
+
+# modulus
+hasil = a % b
+print ("nilai dari a mod b = ", hasil)
+
+#floor dividion (pembagian terdekat-kebawah)
+hasil = a // b
+print ("nilai dari a flor b = ", hasil)
+
